5fa9cddec885fe133d6766cb7fc9058ab0ed1995
luchynets <70798831+luchynets@users.noreply.github.com>
2021-07-28 00:42:43 +0300

    Update settings.py

--- online_shop/online_shop/settings.py
+++ online_shop/online_shop/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,4 +125,4 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+
